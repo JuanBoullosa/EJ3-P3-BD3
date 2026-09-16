f08754f7a6e849377ccfe84d689a6aee45baddce
JuanBoullosa <juanibou@gmail.com>
2026-09-16 10:57:08 -0300

    probando desde pc de trabajo el commit

--- src/accesoBD/Prueba.java
+++ src/accesoBD/Prueba.java
@@ -3,5 +3,6 @@ package accesoBD;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("probando cambios de git jej");
     }
 }
